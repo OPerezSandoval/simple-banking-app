c1d87d392cc3933ea00a716678f50710d8add5eb
OPEREZSANDOVAL <PEREZ.OMAR99@YAHOO.COM>
2024-11-29 13:10:11 -0600

    Add deposit and balance functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  depositMoney,
+  getBalance,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 // Register User
 router.post("/register", registerUser);
 
 // Login User
 router.post("/login", loginUser);
+
+// Deposit Money
+router.post("/deposit", protect, depositMoney);
+
+// Get Balance
+router.get("/balance", protect, getBalance);
 
 module.exports = router;
