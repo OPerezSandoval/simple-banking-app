cef1566a4d48e1b578bc92e8626ce64c263768b8
OPEREZSANDOVAL <PEREZ.OMAR99@YAHOO.COM>
2024-11-29 12:44:32 -0600

    MonogoDb Setup and user functionality

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Please enter a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: 6,
+  },
+});
+
+// Hash the password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
