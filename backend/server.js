0e8e42622b6c3d895a0e63ce9d70f08be52bbf64
OPEREZSANDOVAL <PEREZ.OMAR99@YAHOO.COM>
2024-11-29 12:51:11 -0600

    Setup MongoDb

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+const userRoutes = require("./routes/userRoutes");
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.use("/api/users", userRoutes);
